0563cc0aef5ac4b4e92e7757a3f6db226db7623c
venky0195 <ven.venky08@gmail.com>
2022-07-26 11:58:12 +0530

    Add Server Route(api/hello)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
-    serverMiddleware: ['~/server-middleware/logger'],
-    modules: ['@nuxt/http'],
-    http: {
-      // proxyHeaders: false
-    }
-  }
+  serverMiddleware: { "/": "~/server-middleware/logger", "/api": "~/api" },
+  modules: ["@nuxt/http"],
+  http: {
+    // proxyHeaders: false
+  },
+};
