a7c9ea902bdb7278d4d836fa3345b421ab24c1b3
venky0195 <ven.venky08@gmail.com>
2022-07-26 10:19:03 +0530

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+export default {
+    serverMiddleware: ['~/server-middleware/logger'],
+    modules: ['@nuxt/http'],
+    http: {
+      // proxyHeaders: false
+    }
+  }
---
+++ pages/fun.vue
@@ -0,0 +1,3 @@
+<template>
+<h2>Nuxt is fun!</h2>
+</template>
